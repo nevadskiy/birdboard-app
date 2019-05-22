e22a2b6a803077e510da8c34b0f871e2b0488ff5
Vitalii Kalaida <nevadskiy@gmail.com>
2019-05-22 20:03:01 +0300

    Add styles for projects index page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,16 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="{{ route('projects.create') }}">Create project</a>
+    <header class="flex items-center mb-3 py-2">
+        <h1 class="text-gray-600 mr-auto text-lg">Projects</h1>
+        <a href="{{ route('projects.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create project</a>
+    </header>
 
-    <div class="flex">
+    <main class="lg:flex lg:flex-wrap -mx-2">
         @forelse ($projects as $project)
-            <div class="bg-white mr-4 rounded shadow w-1/3 p-3 overflow-hidden h-40">
-                <h3 class="font-normal text-2xl mb-3 py-3">{{ $project->title }}</h3>
-                <div class="text-gray-500">{{ Str::limit($project->description) }}</div>
+            <div class="lg:w-1/3 px-2 pb-4">
+                <div class="bg-white rounded shadow px-3 py-4 overflow-hidden h-40">
+                    <h3 class="font-normal text-xl mb-3 py-1 px-3 border-blue-400 border-l-4 -ml-3">
+                        <a href="{{ $project->path() }}">{{ $project->title }}</a>
+                    </h3>
+                    <div class="text-gray-500">{{ Str::limit($project->description) }}</div>
+                </div>
             </div>
         @empty
             <div>No projects yet.</div>
         @endforelse
-    </div>
+    </main>
 @endsection
