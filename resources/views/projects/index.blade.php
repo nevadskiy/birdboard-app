b2e6345989908aa5db9151fd357a83644ab475d7
Vitalii Kalaida <nevadskiy@gmail.com>
2019-05-22 21:52:45 +0300

    Add styles for project show page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,20 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <header class="flex items-center mb-3 py-2">
-        <h1 class="text-gray-600 mr-auto text-lg">Projects</h1>
+    <header class="flex items-end mb-3 py-2">
+        <h2 class="text-gray-600 mr-auto text-lg">
+            <a href="{{ route('projects.index') }}">Projects</a>
+        </h2>
         <a href="{{ route('projects.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create project</a>
     </header>
 
     <main class="lg:flex lg:flex-wrap -mx-2">
         @forelse ($projects as $project)
-            <div class="lg:w-1/3 px-2 pb-4">
-                <div class="bg-white rounded shadow px-3 py-4 overflow-hidden h-40">
-                    <h3 class="font-normal text-xl mb-3 py-1 px-3 border-blue-400 border-l-4 -ml-3">
-                        <a href="{{ $project->path() }}">{{ $project->title }}</a>
-                    </h3>
-                    <div class="text-gray-500">{{ Str::limit($project->description) }}</div>
-                </div>
+            <div class="lg:w-1/3 px-2 mb-4">
+                @include('projects._card')
             </div>
         @empty
             <div>No projects yet.</div>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,6 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $project->title }}</h1>
-    <p>{{ $project->description }}</p>
+    <header class="flex items-end mb-3 py-2">
+        <p class="text-gray-600 mr-auto text-lg">
+            <a href="{{ route('projects.index') }}">My projects</a> / {{ $project->title }}
+        </p>
+        <a href="{{ route('projects.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create project</a>
+    </header>
+
+    <main>
+        <div class="lg:flex -mx-3">
+            <div class="lg:w-3/4 px-3 mb-6">
+                <div class="mb-8">
+                    <h2 class="text-gray-600 text-lg mb-2">Tasks</h2>
+                    <div class="card mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto blanditiis consectetur delectus, distinctio dolorem dolorum earum eligendi facilis hic incidunt ipsam laborum molestias neque nisi obcaecati quae quidem rem soluta?</div>
+                    <div class="card mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto blanditiis consectetur delectus, distinctio dolorem dolorum earum eligendi facilis hic incidunt ipsam laborum molestias neque nisi obcaecati quae quidem rem soluta?</div>
+                    <div class="card mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto blanditiis consectetur delectus, distinctio dolorem dolorum earum eligendi facilis hic incidunt ipsam laborum molestias neque nisi obcaecati quae quidem rem soluta?</div>
+                    <div class="card mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto blanditiis consectetur delectus, distinctio dolorem dolorum earum eligendi facilis hic incidunt ipsam laborum molestias neque nisi obcaecati quae quidem rem soluta?</div>
+                </div>
+
+                <h2 class="text-gray-600 text-lg mb-2">General notes</h2>
+
+                <textarea class="card w-full h-56">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto blanditiis consectetur delectus, distinctio dolorem dolorum earum eligendi facilis hic incidunt ipsam laborum molestias neque nisi obcaecati quae quidem rem soluta?</textarea>
+            </div>
+
+            <div class="lg:w-1/4 px-3">
+                @include('projects._card')
+            </div>
+        </div>
+    </main>
 @endsection
